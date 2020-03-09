86b2a651b3cc188fef46301ad6b48bf3ab6503f8
guzongjia <kabuto412rock@gmail.com>
2020-03-09 12:09:21 +0800

    Fix: Super class tha you want to override in the same file will be ok

--- src/generate /overrideBase.ts
+++ src/generate /overrideBase.ts
@@ -1,13 +1,13 @@
 import * as vscode from 'vscode';
 import { getMethodInformationFromClassContent, getClassInformationFromEditorCursor, setCursorInCurrentTextEditor, jsonCopy } from './util';
 import { format } from 'path';
+import { stringify } from 'querystring';
 
 export function generateOverrideDisposable(): vscode.Disposable {
-    let disposable = vscode.commands.registerCommand("extension.overrideMethods", async function() {
-        // Works below can't work in preview mode
-        // vscode.commands.executeCommand("workbench.action.files.save");
- 
+    let disposable = vscode.commands.registerCommand("extension.overrideMethods", async function () {
+        // 0. Keep Editor or your current edit will be broken by command "goToSuper".
         await vscode.commands.executeCommand("workbench.action.keepEditor");
+
         // 1. Get information include the class's range and class scope string in the document
         let information = getClassInformationFromEditorCursor();
 
@@ -15,37 +15,74 @@ export function generateOverrideDisposable(): vscode.Disposable {
             vscode.window.showErrorMessage("Where is the Class?I can't find it.");
             return;
         }
-        vscode.window.showInformationMessage("class name is", information.className);
+        // vscode.window.showInformationMessage("class name is", information.className);
 
+        // TODO: Check the super class is in the same file or not
+        console.log("classNamesInThisFile:", information.classNamesInThisFile);
+        console.log("information.extendString:", information.extendString);
+
+        let isSuperClassSameFile: boolean = false;
+        let extendClassName: string;
+        let extendClassNameIndex: number = 0;
+        if (information.extendString !== "") {
+            extendClassName = information.extendString;
+            console.log("Your extend class:'", extendClassName, "'");
+            information.classNamesInThisFile.forEach(someClass => {
+
+            });
+            information.classNamesInThisFile.forEach((someClass, index, arr) => {
+                console.log("Another claas in same file:'", someClass, "'");
+                if (someClass === extendClassName) {
+                    console.log("同樣的");
+                    isSuperClassSameFile = true;
+                    if (information !== null) {
+                        extendClassNameIndex = information.classNamesInThisFileIndices[index];
+                    }
+                }
+            });
+        }
+        console.log("isSuperClassSameFile:", isSuperClassSameFile);
         let extension = vscode.extensions.getExtension("dart-code.dart-code");
         if (extension === undefined || extension.isActive === false) {
             vscode.window.showErrorMessage("You should install the extension 'Dart' from 'Dart Code'");
             return;
         }
-        
-        extension.activate().then(async function () {
-            await vscode.commands.executeCommand("dart.goToSuper").then(async function () {
 
-                let superClassInformation = getClassInformationFromEditorCursor();
-                if (superClassInformation === null) {
-                    vscode.window.showErrorMessage("Super class is not really open... Maybe you should try to issue or fork author's project solve this by yourself.")
+        extension.activate().then(async function () {
+            if (!isSuperClassSameFile) {
+                await vscode.commands.executeCommand("dart.goToSuper");
+            }
+            if (isSuperClassSameFile) {
+                let editor = vscode.window.activeTextEditor;
+                
+                if (editor === undefined) {
+                    vscode.window.showErrorMessage("You should install the extension 'Dart' from 'Dart Code'");
                     return;
                 }
+                // move cursor to your super class when it's in the same file
+                let extendClassNamePosition = editor.document.positionAt(extendClassNameIndex);
+                setCursorInCurrentTextEditor(extendClassNamePosition);
+            }
+            let superClassInformation = getClassInformationFromEditorCursor();
+            if (superClassInformation === null) {
+                vscode.window.showErrorMessage("Super class is not really open... Maybe you should try to issue or fork author's project solve this by yourself.")
+                return;
+            }
+            if (!isSuperClassSameFile) {
                 await vscode.commands.executeCommand("workbench.action.closeActiveEditor");
-
-                console.log("#class@", superClassInformation.className);
-                console.log("#class#", superClassInformation.classContent);
-                getMethodInformationFromClassContent(superClassInformation.classContent);
-            });
+            }
+            console.log("#class@", superClassInformation.className);
+            console.log("#class#", superClassInformation.classContent);
+            getMethodInformationFromClassContent(superClassInformation.classContent);
 
         });
 
-        // 
+        //  
         vscode.window.showInformationMessage("DartKu: OverrideMethods");
 
     });
     return disposable;
 }
 
-async function handleOverride(isSuperClassInSameFile: boolean){
+async function handleOverride(isSuperClassInSameFile: boolean) {
 }
--- src/generate /util.ts
+++ src/generate /util.ts
@@ -25,6 +25,8 @@ export type ClassCotentInformation = {
     className: string,
     extendString: string,
     implementString: string,
+    classNamesInThisFile: string[],
+    classNamesInThisFileIndices: number[],
 };
 
 export function setCursorInCurrentTextEditor(newPosition: Position) {
@@ -59,28 +61,36 @@ export function getClassInformationFromEditorCursor(): ClassCotentInformation | 
     editor.selection = newSelection;
 
     const allText = editor.document.getText();
-
+    let allClassNames: string[] = [];
+    let allClassNamesIndices: number[] = [];
     // Find strings that match pattern like "class ... {" in all text string.
     // 找到所有符合"class ... {"
     // const allMatchArray = allText.match(/class .+{/g);
     const classRe = /(abstract class|class)\s([_\w\d]+)?(<([_\w\d\s,]+)>)?(\s*extends\s(<?[_<>,\w\d]+)>?)?\s*(implements\s(([_<>\w\d]+)\s?,?)+)?\s*{\n?/g;
     const cursorIndex = editor.document.offsetAt(newCursorPosition);
     let classCount = 0;
-    let startIndex = 0;
     let realStartIndex = 0;
-    let endIndex = 0;
+    let realEndIndex = 0;
     let isClassFind: boolean = false;
     let match;
-    let realClassName: string = "";
-    let realExtendString: string = "";
-    let realImplementString: string = "";
+
+    let resultStartIndex: number = 0;
+    let resultEndIndex: number = 0;
+    let resultClassName: string = "";
+    let resultExtendString: string = "";
+    let resultImplementString: string = "";
+
     let allMatchClassNames;
     console.log("\n\n@@@@@@@@@@@@");
     while ((match = classRe.exec(allText)) !== null) {
         let bracketScore = 0;
         let matchClassName = match[2];
+        // add my class names
+        allClassNames.push(matchClassName);
+
         realStartIndex = match.index;
-        startIndex = allText.indexOf('{', realStartIndex);
+        let startIndex = allText.indexOf('{', realStartIndex);
+        allClassNamesIndices.push(startIndex);
         bracketScore = 0;
         for (let index = startIndex; index < allText.length; index++) {
             const ch = allText[index];
@@ -93,41 +103,43 @@ export function getClassInformationFromEditorCursor(): ClassCotentInformation | 
                     break;
             }
             if (bracketScore === 0) {
-                endIndex = index;
+                realEndIndex = index;
                 classCount += 1;
                 break;
             }
         }
 
-        console.log("cursorIndex = " + cursorIndex + " realStartIndex=" + realStartIndex + ", endIndex=" + endIndex);
+        console.log("cursorIndex = " + cursorIndex + " realStartIndex=" + realStartIndex + ", realEndIndex=" + realEndIndex);
         let cursorPosition = editor.document.positionAt(cursorIndex);
         console.log("line:", cursorPosition.line, ", char:", cursorPosition.character);
-        if (cursorIndex >= realStartIndex && cursorIndex <= endIndex) {
+        if (cursorIndex >= realStartIndex && cursorIndex <= realEndIndex &&isClassFind === false) {
             // find the class's range
             let realStartPosition = editor.document.positionAt(realStartIndex);
             console.log("realStatIndex:", realStartIndex, ", line:", realStartPosition.line, ", char:", realStartPosition.character);            
-            console.log("符合條件:", cursorIndex, " >= ", realStartIndex, " && ", cursorIndex, " <= ", endIndex);
-            realClassName = matchClassName;// Just this class's name
+            console.log("符合條件:", cursorIndex, " >= ", realStartIndex, " && ", cursorIndex, " <= ", realEndIndex);
+            resultClassName = matchClassName;// Just this class's name
             if (match[6] !== undefined) {
-                realExtendString = match[6].trim();// looks like "extends someClass123"
+                resultExtendString = match[6].trim();// looks like "extends someClass123"
             }
             if (match[7] !== undefined) {
-                realImplementString = match[7].trim();// looks like "implements aClass, bClass"
+                resultImplementString = match[7].trim();// looks like "implements aClass, bClass"
             }
-            console.log("realClassName:'", realClassName, "'");
-            console.log("realExtendString:'", realExtendString, "'");
-            
-            console.log("realImplementString:'", realImplementString, "'");
-
+            resultStartIndex = realStartIndex;
+            resultEndIndex = realEndIndex;
+            console.log("resultClassName:'", resultClassName, "'");
+            console.log("resultExtendString:'", resultExtendString, "'");
+            console.log("resultImplementString:'", resultImplementString, "'");
             isClassFind = true;
-            break;
         } else {
-            console.log("不符合條件:", cursorIndex, " >= ", realStartIndex, " && ", cursorIndex, " <= ", endIndex);
+            console.log("不符合條件:", cursorIndex, " >= ", realStartIndex, " && ", cursorIndex, " <= ", realEndIndex);
+            console.log("XresultClassName:'", resultClassName, "'");
+            console.log("XresultExtendString:'", resultExtendString, "'");
+            console.log("XresultImplementString:'", resultImplementString, "'");
         }
 
 
     }
-    console.log("你逃出了while迴圈cursorIndex:", cursorIndex, " >= ", realStartIndex, " && ", cursorIndex, " <= ", endIndex);
+    // console.log("你逃出了while迴圈cursorIndex:", cursorIndex, " >= ", realStartIndex, " && ", cursorIndex, " <= ", endIndex);
     if (classCount === 0) {
         window.showWarningMessage("沒有class你還想叫我產生建構子...");
         console.log("%沒有class你還想叫我產生建構子...");
@@ -135,21 +147,23 @@ export function getClassInformationFromEditorCursor(): ClassCotentInformation | 
         return null;
     }
 
-    if (isClassFind === false || match === null) {
+    if (isClassFind === false) {
         // console.log("%沒有找到對應的class, Sorry, isClassFind:", isClassFind, ",match:", match);
         return null;
     }
-    let classContent = allText.substring(realStartIndex, endIndex);
+    let classContent = allText.substring(resultStartIndex, resultEndIndex);
     // 印出cursor所在的Class內容範圍
-    console.log(classContent);
-
+    console.log("@@classContent@@\n",classContent);
+    
     return {
-        startPosition: editor.document.positionAt(realStartIndex),
-        endPosition: editor.document.positionAt(endIndex),
+        startPosition: editor.document.positionAt(resultStartIndex),
+        endPosition: editor.document.positionAt(resultEndIndex),
         classContent: classContent,
-        className: realClassName,
-        extendString: realExtendString,
-        implementString: realImplementString,
+        className: resultClassName,
+        extendString: resultExtendString,
+        implementString: resultImplementString,
+        classNamesInThisFile: allClassNames,
+        classNamesInThisFileIndices: allClassNamesIndices,
     };
 
 }
